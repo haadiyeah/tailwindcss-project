af87d1f21a050464efce8436d2eaa5b82a24c267
haadiyeah <pazocado@gmail.com>
2024-06-28 11:27:27 +0500

    initial project setup with react, tailwind & typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ types/src/assets/svg/index.d.ts
@@ -0,0 +1,6 @@
+// types/src/assets/svg/index.d.ts
+declare module '*.jsx' {
+  import { FC } from 'react';
+  const component: FC<any>;
+  export default component;
+}
