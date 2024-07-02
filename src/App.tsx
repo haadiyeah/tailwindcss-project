1531e637837bc914d3fe9b7191a8202a5468d69e
haadiyeah <pazocado@gmail.com>
2024-07-02 12:59:24 +0500

    hero section completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Button from "./components/Button";
 import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
 
 const App = () => {
   return (
     <>
-      <div className="pt-4 lg:pt-5 overflow-hidden ring-1 ring-red-600">
+      <div className="pt-4 lg:pt-5 overflow-hidden">
         <Header />
+        <Hero />
       </div>
 
     </>
